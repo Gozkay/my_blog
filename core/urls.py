b113330b571978e116c3b6ed62af71995b51662b
Chigozie Emmanuel <stompiddo3@email.com>
2026-06-17 09:54:49 +0100

    blog and foood view now responsive

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,8 @@
 from django.urls import include, path
 
+from core import views
+
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('category/<str:name>/', views.category, name='category'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from core.models import Article, Category, Comment
 
 
 # Create your views here.
+def home(request):
+    articles = Article.objects.all()
+    categories = Category.objects.all()
+    comments = Comment.objects.all()
+
+    context = {
+        'articles': articles,
+        'categories': categories,
+        'comments': comments
+    }
+
+    return render(request, 'post_list.html', context)
+
+def category(request,name):
+    category = Category.objects.get(name=name)
+    Categories = Category.objects.all()
+
+
+    context = {
+        'category': category,
+        'categories': Categories
+        
+    }
+
+    return render(request, 'category_detail.html', context)
